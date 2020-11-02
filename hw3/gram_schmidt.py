bea2f0aceeb26b3fc61378a1d2ce6ed80cbf12d3
Briar Doty <briardoty@gmail.com>
2020-11-02 09:23:15 -0800

    Implement QR decomp

--- hw3/gram_schmidt.py
+++ hw3/gram_schmidt.py
@@ -3,6 +3,15 @@ import numpy as np
 from scipy import linalg
 import math
 
+def qr_decomp(A):
+    """
+    Perform QR decomposition of A using gram-shmidt
+    """
+    Q = gram_schmidt(A)
+    R = build_R(A, Q)
+
+    return Q, R
+
 def gram_schmidt(A):
     """
     Orthonormalize the matrix A via modified gram-schmidt
@@ -13,7 +22,7 @@ def gram_schmidt(A):
     for i in range(A.shape[1]):
         
         # select column
-        q = A[:,i]
+        q = np.array(A[:,i])
         
         # make orthogonal to all previous cols
         proj = np.zeros_like(q)
@@ -29,7 +38,30 @@ def gram_schmidt(A):
     
     return Q
 
+def build_R(A, Q):
+    """
+    Build upper triangular R from A = QR decomp
+    """
+    m, n = Q.shape
+    R = np.zeros((n,n))
+
+    # build
+    for i in range(n):
+        for j in range(n):
+            R[i,j] = np.inner(Q[:,i], A[:,j])
+
+    return R
+
 def main():
+
+    A = np.array([
+        [1, 0, -2],
+        [0.5, 1, 0],
+        [0.3, -1, -6],
+        [0, 1.2, 0]
+    ])
+
+    Q, R = qr_decomp(A)
 
     return
 
